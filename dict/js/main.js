1c05433f8a733ea49a7b7e67c0b5b0a0d0eff675
chihaoyo <chihaoyo@gmail.com>
2016-06-03 19:06:47 +0800

    Refine ui

--- dict/js/main.js
+++ dict/js/main.js
@@ -17,15 +17,33 @@ _fb_terms.on('child_added', function(snapshot) {
 });
 
 // ui
+var $body;
 var $term_list;
 var ui_add_term = function(term) {
-  var $li = $('<li>').appendTo($term_list);
-  $('<h1>').html(term).appendTo($li);
+  var $section = $('<section>').appendTo($term_list);
+  $('<h1>').html(term).appendTo($section);
+  $('<a>').addClass('explain').attr('href', '#').html('<span class="activate">explain this...</span><span class="activated">explaining this...</span>').click(function() {
+    var $a = $(this);
+    var $f = $('form#add_explanation').detach();
+    $a.toggleClass('active');
+    if($a.hasClass('active')) {
+      $('a.explain').not($a).removeClass('active');
+      $f.removeClass('hide').insertBefore($a);
+    }
+    else {
+      $f.addClass('hide').appendTo($body);
+    }
+    return false;
+  }).appendTo($section);
 }
 
 // ui callback binding
 $(function() {
+  // selecting
+  $body = $('body');
   $term_list = $('#term_list');
+
+  // binding
   $('form#add_term').submit(function() {
     var $this = $(this);
     var term = $this.find('[name="term"]').val();
